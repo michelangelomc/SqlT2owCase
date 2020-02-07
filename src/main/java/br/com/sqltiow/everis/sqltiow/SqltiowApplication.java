491157c09617c3bdbb341ca00bb70cbc818714c6
Michelangelo Mendes Chaves <mickelchaves@hotmail.com>
2020-02-07 10:52:43 -0300

    first commit

---
+++ src/main/java/br/com/sqltiow/everis/sqltiow/SqltiowApplication.java
@@ -0,0 +1,13 @@
+package br.com.sqltiow.everis.sqltiow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqltiowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqltiowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sqltiow/everis/sqltiow/SqltiowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sqltiow.everis.sqltiow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqltiowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
